e0463ffb46053cb2e0ab00b8d17968a83e5444c6
Luciano Filho <lvgalvaofilho@gmail.com>
2023-03-21 07:43:14 -0300

    create a new loop to itercolumns

--- geocodepandas/__init__.py
+++ geocodepandas/__init__.py
@@ -1,4 +1,4 @@
-from reverse_geocoder_geojson import ReverseGeocoder
+from reverse_geocoder import ReverseGeocoder
 from read_csv import CSVReader
 import requests
 
@@ -7,22 +7,7 @@ df = reader.read_csv()
 df_filtered = df.where(df['country'] == 'MX').dropna()
 print(df_filtered.count())
 
-# create empty lists to store the address components
-displayed_names = []
-roades = []
-categories = []
-types = []
-houses_number = []
-cities = []
-states = []
-suburbes = []
-postcodes = []
-osm_types = []
-osm_ides = []
-openstreetmap_urles = []
-
-failed_indices = []
-
+column_names = ['display_name', 'road', 'house_number', 'city', 'state', 'suburb', 'postcode', 'category', 'type', 'osm_type', 'osm_id']
 
 for index, row in df_filtered.iterrows():
     # get the latitude and longitude values from the DataFrame
@@ -41,54 +26,10 @@ for index, row in df_filtered.iterrows():
             print(f"Error at index {index}: {e}")
         break
     
-    # get the address components using the ReverseGeocoder class
-    # {'category': category, 'type': type, 'display_name': display_name, 'road': road, 'house_number': house_number, 'suburb': suburb, 'city': city, 'state': state, 'postcode': postcode}
-    display_name = address_components.get('display_name')
-    road = address_components.get('road')
-    house_number = address_components.get('house_number')
-    city = address_components.get('city')
-    state = address_components.get('state')
-    suburb = address_components.get('suburb')
-    postcode = address_components.get('postcode')
-    category = address_components.get('category')
-    type = address_components.get('type')
-    osm_type = address_components.get('osm_type')
-    osm_id = address_components.get('osm_id')
-
-    # append the address components to the lists
-    displayed_names.append(display_name)
-    categories.append(category)
-    types.append(type)
-    roades.append(road)
-    houses_number.append(house_number)
-    cities.append(city)
-    states.append(state)
-    suburbes.append(suburb)
-    postcodes.append(postcode)
-    osm_types.append(osm_type)
-    osm_ides.append(osm_id)
-    openstreetmap_urles.append(f'https://www.openstreetmap.org/{osm_type}/{osm_id}')
-
-# assign the address components lists to new columns in the DataFrame
-df_filtered['display_name'] = displayed_names
-df_filtered['road'] = roades
-df_filtered['house_number'] = houses_number
-df_filtered['city'] = cities
-df_filtered['state'] = states
-df_filtered['suburb'] = suburbes
-df_filtered['postcode'] = postcodes
-df_filtered['category'] = categories
-df_filtered['type'] = types
-df_filtered['osm_type'] = osm_types
-df_filtered['osm_id'] = osm_ides
-df_filtered['openstreetmap'] = openstreetmap_urles
-
-
-
-# print the updated DataFrame
+    # get the address components using the ReverseGeocoder class and append the information to the dataframe
+    for column_name in column_names:
+        df_filtered.at[index, column_name] = address_components.get(column_name)
 
 loader = CSVReader.save_csv('output/updated_stores_mx.csv', df_filtered, 'output/updated_stores_mx.csv')
 
-if failed_indices:
-    print(f"Failed to get address for the following indices: {failed_indices}")
 
--- geocodepandas/reverse_geocoder.py
+++ geocodepandas/reverse_geocoder.py
@@ -4,7 +4,7 @@ class ReverseGeocoder:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
-        self.url = f'https://nominatim.openstreetmap.org/reverse?format=json&lat={lat}&lon={lon}'
+        self.url = f'https://nominatim.openstreetmap.org/reverse?format=geojson&lat={lat}&lon={lon}'
 
     def get_address(self):
         # send a GET request to the API endpoint
@@ -14,10 +14,16 @@ class ReverseGeocoder:
         data = response.json()
 
         # get the address components
-        address = data['display_name']
-        city = data['address'].get('city')
-        state = data['address'].get('state')
-        state_district = data['address'].get('state_district')
-        postcode = data['address'].get('postcode')
+        display_name = data['features'][0]['properties']['display_name']
+        category = data['features'][0]['properties']['category']
+        type = data['features'][0]['properties']['type']
+        osm_id = data['features'][0]['properties']['osm_id']
+        osm_type = data['features'][0]['properties']['osm_type']
+        road = data['features'][0]['properties']['address'].get('road')
+        house_number = data['features'][0]['properties']['address'].get('house_number')
+        suburb = data['features'][0]['properties']['address'].get('suburb')
+        city = data['features'][0]['properties']['address'].get('city')
+        state = data['features'][0]['properties']['address'].get('state')
+        postcode = data['features'][0]['properties']['address'].get('postcode')
 
-        return {'address': address, 'city': city, 'state': state, 'state_district': state_district, 'postcode': postcode}
+        return {'category': category, 'type': type, 'display_name': display_name, 'osm_type': osm_type, 'osm_id': osm_id, 'road': road, 'house_number': house_number, 'suburb': suburb, 'city': city, 'state': state, 'postcode': postcode}
